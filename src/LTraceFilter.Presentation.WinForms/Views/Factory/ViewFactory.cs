d5248a5c890334e6a3026fadc5b33a08e98df0ae
GabrielDaltro <gabriel.d.8@hotmail.com>
2023-06-26 21:15:56 -0300

    created SignalFiltering View

---
+++ src/LTraceFilter.Presentation.WinForms/Views/Factory/ViewFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace LTraceFilter.Presentation.WinForms.Views
+{
+    internal class ViewFactory
+    {
+        private readonly IServiceProvider serviceProvider;
+
+        public ViewFactory(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        public SignalFilteringView CreateSignalFilteringView()
+        {
+            return serviceProvider.GetRequiredService<SignalFilteringView>();
+        }
+    }
+}
---
+++ src/LTraceFilter.Presentation.WinForms/Views/SignalFilteringView.cs
@@ -0,0 +1,14 @@
+﻿using LTraceFilter.Presentation.WinForms.Presenters;
+
+namespace LTraceFilter.Presentation.WinForms.Views
+{
+    internal partial class SignalFilteringView : UserControl
+    {
+        private readonly SignalFilteringPresenter presenter;
+        public SignalFilteringView(SignalFilteringPresenter presenter)
+        {
+            this.presenter = presenter;
+            InitializeComponent();
+        }
+    }
+}
